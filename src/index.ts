71b3171d41104f360cc169acf41e295f1d3c7bc4
Armando Peralta <armandoperalta@webdesignnodes.com>
2025-11-08 23:02:19 -0600

    Dev: DotEnv Dependencie Implementation.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 
+// * Dependencies Required
+
+import dotenv from "dotenv";
+
+// * Initializing DotEnv
+
+dotenv.config({ path: ".env"});
+
+console.log(process.env.SITES_LIST);
+
+
 // * Modules Required
 
-import News_Extracter from "./New-Extracter";
+// import News_Extracter from "./New-Extracter";
 // import Dictionary_Contructor from "./Dictionary";
 
-News_Extracter.init();
+// News_Extracter.init();
 
 // Dictionary_Contructor.init();
